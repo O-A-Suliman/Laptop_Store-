ccdde5ee1424b78e3e9472117940e69d7bc2a2e0
Omer Ahmed <ayahyam18@gmail.com>
2026-04-03 10:19:15 +0200

    d

--- products/models.py
+++ products/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name=models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
 
 class Product(models.Model):
     name=models.CharField(max_length=255)
@@ -10,3 +12,5 @@ class Product(models.Model):
     description=models.TextField()
     price=models.DecimalField(max_digits=10,decimal_places=2)
     image=models.ImageField(upload_to='products')
+    def __str__(self):
+        return self.name
