f4961fae3d86be4cd3432554ec5965fff3f52e43
Omer Ahmed <ayahyam18@gmail.com>
2026-04-03 10:12:34 +0200

    Ignore virtual environment

---
+++ products/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from .
+
+# Create your models here.
+class Category(models.Model):
+    name=models.CharField(max_length=255)
+
+class Product(models.Model):
+    name=models.CharField(max_length=255)
+    category=models.ForeignKey(Category,related_name="products")
+    description=models.TextField()
+    price=models.DecimalField(max_digits=10,decimal_places=2)
+    image=models.ImageField(upload_to='products')
