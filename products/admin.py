bcb2a3d84d1c470d221a73ff886267176c4fed92
Omer Ahmed <ayahyam18@gmail.com>
2026-04-05 13:30:13 +0200

    D

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 from .models import Product,Category,StoreSetting
 # Register your models here.
-admin.site.register(Product)
+@admin.register(Product)
+class AdminProduct(admin.ModelAdmin):
+    list_display=["name","category","price","stack"]
+    list_filter=['category',]
+    fieldsets=(
+        ('Basic data',{
+            'fields':("name", "price", "description",'stack')
+            }),
+        ("Technical specifications",{
+            'fields':('CPU','RAM','storage','GPU'),
+        })
+    )
+
 admin.site.register(Category)
 @admin.register(StoreSetting)
 class AdminStoreSetting(admin.ModelAdmin):
--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
 class Category(models.Model):
@@ -12,12 +13,33 @@ class Product(models.Model):
     description=models.TextField(null=True,blank=True)
     price=models.DecimalField(max_digits=10,decimal_places=2)
     image=models.ImageField(upload_to='products',null=True,blank=True)
+    CPU=models.CharField(max_length=255)
+    RAM=models.CharField(max_length=255)
+    storage=models.CharField(max_length=255)
+    GPU=models.CharField(max_length=255)
+    stack=models.IntegerField(default=0)
     def __str__(self):
         return self.name
     
 class StoreSetting(models.Model):
     name=models.CharField(max_length=255)
     description=models.TextField(null=True,blank=True)
-    whatsapp_number=models.CharField(max_length=255)
+    whatsapp_number=PhoneNumberField()
     facebook_link=models.CharField(max_length=255)
     logo=models.ImageField(upload_to='products',null=True,blank=True)
+    def __str__(self):
+        return self.name
+    
+class Order(models.Model):
+    class Status(models.TextChoices):
+        Pending='PENDING',"Pending"
+        InProgress='INPROGRESS','In Progress'
+        Completed='COMPLETED','Completed'
+        Cancelled='CANCELLED','Cancelled'
+    name=models.CharField(max_length=255)
+    phone=PhoneNumberField()
+    product=models.ForeignKey(Product,related_name='orders',on_delete=models.CASCADE)
+    date_order=models.DateTimeField(auto_now_add=True)
+    status=models.CharField(choices=Status.choices,default=Status.InProgress)
+    def __str__(self):
+        return self.name
